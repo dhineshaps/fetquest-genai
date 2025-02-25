896e273d9d4b17a218cad319fc071f948f920e2e
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-25 14:27:01 +0530

    Update ai_agent.py

--- pages/ai_agent.py
+++ pages/ai_agent.py
@@ -1,8 +1,8 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
-from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.googlesearch import GoogleSearch
+from phi.tools.duckduckgo import DuckDuckGo
 from dotenv import load_dotenv
 import time
 from typing import Iterator 
