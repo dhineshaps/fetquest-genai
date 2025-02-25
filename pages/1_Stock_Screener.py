261696878bd92ab076fc9a2c65e5120a11576af2
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-25 16:06:48 +0530

    Added .NS to scrip concat

--- pages/1_Stock_Screener.py
+++ pages/1_Stock_Screener.py
@@ -267,11 +267,11 @@ def output_display(pr_hld,qtr,sales,qtrs,opm,qts):
     #     st.pyplot(fig5)
     #     st.info("EPS Increasing along with Price of the stock shows the steady earning and justifiable Stock Price")
 def agent_ai(scrip):
-      query = f"Provide a fundamental analysis for {scrip}."
+      query = f"Provide a fundamental analysis for {scrip} + ".NS" ."
       chunks = finance_agent.run(query, stream=True)
       filtered_chunks = (chunk for i, chunk in enumerate(as_stream(chunks)) if i >= 3)
       with st.container():    
-           st.write("Space for Agentic Container" + scrip)
+           st.write("Space for Agentic Container " + scrip)
            response = st.write_stream(filtered_chunks)
 
 
