6a44d982c5ca61f03aa3dfddfbb9139807bd505f
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-27 16:41:45 +0530

    Update the gemini model 5_PDF_Report_Analyzer.py

--- pages/5_PDF_Report_Analyzer.py
+++ pages/5_PDF_Report_Analyzer.py
@@ -81,7 +81,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",
                              temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
