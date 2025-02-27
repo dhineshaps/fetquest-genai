3cd336752a2afc32c3ef4a1f5a072f052642a7fe
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-27 16:55:14 +0530

    Update 5_PDF_Report_Analyzer.py

--- pages/5_PDF_Report_Analyzer.py
+++ pages/5_PDF_Report_Analyzer.py
@@ -82,7 +82,7 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",
-                             temperature=0.3)
+                             temperature=0.3, allow_dangerous_deserialization=True)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
