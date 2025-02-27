038e1f331ea7b63d6a91ed05c9c498fb7bde4fe3
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-27 17:17:18 +0530

    updated true for decentralization

--- pages/5_PDF_Report_Analyzer.py
+++ pages/5_PDF_Report_Analyzer.py
@@ -82,7 +82,7 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",
-                             temperature=0.3, allow_dangerous_deserialization=True)
+                             temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
@@ -92,7 +92,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
