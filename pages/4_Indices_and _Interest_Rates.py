526fbb9f65cca4d82bb18146b4e48dfaedf3a6f8
Dhinesh Palanisamy <dhinesh.palanisamy@yahoo.com>
2025-03-16 02:21:35 +0530

    side bar with pages added for all pages except screener

--- pages/4_Indices_and _Interest_Rates.py
+++ pages/4_Indices_and _Interest_Rates.py
@@ -8,6 +8,17 @@ from PIL import Image
 im = Image.open('the-fet-quest.jpg')
 st.set_page_config(page_title="Indices", page_icon = im,layout="wide")
 
+with st.sidebar: 
+    st.markdown(":blue[Services:]")
+    st.sidebar.page_link('pages/1_AI_Stock_Screener.py', label='AI Stock Screener')
+    st.sidebar.page_link('pages/2_Chatbot.py', label='Chatbot')
+    st.sidebar.page_link('pages/3_Imagebot.py', label='Imagebot')
+    st.sidebar.page_link('pages/4_Indices_and _Interest_Rates.py', label='Indices and Interest_Rate')
+    st.sidebar.page_link('pages/5_PDF_Report_Analyzer.py', label='PDF Report Analyzer')
+    st.sidebar.page_link('pages/6_About_us_And_FAQs.py', label='About us And FAQs')
+    st.divider()
+    st.sidebar.image("the-fet-quest.jpg")
+
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
       new_title = '<p style="font-family:fantasy; color:#DAA520; font-size: 32px;">Indices and Interest rates 📈</p>'
