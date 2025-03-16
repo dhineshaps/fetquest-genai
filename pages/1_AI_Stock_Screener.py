3267c2b1e377438c814e1034aa2c90eb3db6fd78
Dhinesh Palanisamy <dhinesh.palanisamy@yahoo.com>
2025-03-16 15:36:11 +0530

    homepage navigation test v12

--- pages/1_AI_Stock_Screener.py
+++ pages/1_AI_Stock_Screener.py
@@ -87,7 +87,7 @@ today = date.today()
 # col_one_list = df['SYMBOL'].tolist()
 
 if st.sidebar.button("🏠 Home"):
-    st.experimental_set_query_params(page="Home.py")  # Optional: Set query param
+    st.query_params["page"] = "home"  # Set query param
     st.rerun() 
 
 with st.sidebar: 
