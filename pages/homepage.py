d3ef70d6e73efa4aea81c938f0b0f56b67ba376d
Dhinesh Palanisamy <dhinesh.palanisamy@yahoo.com>
2025-03-22 14:36:15 +0530

    side page navigation for sectoral stocks added for all pages

--- pages/homepage.py
+++ pages/homepage.py
@@ -7,6 +7,7 @@ left_co, cent_co,last_co = st.columns(3)
 with st.sidebar: 
     st.markdown(":blue[Services:]")
     st.sidebar.page_link('pages/1_AI_Stock_Screener.py', label='AI Stock Screener')
+    st.sidebar.page_link('pages/sectoral_stock.py', label='Sectoral Stocks')
     st.sidebar.page_link('pages/2_Chatbot.py', label='Chatbot')
     st.sidebar.page_link('pages/3_Imagebot.py', label='Imagebot')
     st.sidebar.page_link('pages/4_Indices_and _Interest_Rates.py', label='Indices and Interest_Rate')
