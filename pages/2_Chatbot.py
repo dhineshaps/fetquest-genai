007b6d8759b89e3bee85f36b1a37aa4194eb598e
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-05-20 22:44:30 +0530

    changed the model

--- pages/2_Chatbot.py
+++ pages/2_Chatbot.py
@@ -56,7 +56,8 @@ genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 st.info("Chatbot is powered by Google Gemini Pro and have trained Data till Sep 2021")
 ## FUNCTION TO LOAD Gemini Pro model and get response
 
-model = genai.GenerativeModel("gemini-1.5-pro-latest")  #for text - gemini pro
+#model = genai.GenerativeModel("gemini-1.5-pro-latest")  #for text - gemini pro
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 def get_gemini_response(question):
 
